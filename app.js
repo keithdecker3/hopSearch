95e7f3f36290ff71a1c7d1d0a462b2601652b23d
Jacob Hinkston <JacobHinkston@Gmail.com>
2018-04-27 14:34:26 -0600

    working js and basic website functionality

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 randomUrl = 'https://api.punkapi.com/v2/beers/random'
 
 function addBeerData(data) {
-  document.querySelector('.beer-name').innerText = 
+  document.querySelector('.beer-name').innerText = `${data[0].name} - ${data[0].abv} ABV - ${data[0].ibu} IBU`
   document.querySelector('.description').innerText = data[0].description
   data[0].ingredients.malt.forEach(malt => {
     let maltList = document.querySelector('.malt')
     let maltName = document.createElement('li')
-    maltName.innerText = 
+    maltName.innerText = `Name: ${malt.name}, Amount: ${malt.amount.value} ${malt.amount.unit}`
     maltList.appendChild(maltName)
   })
   data[0].ingredients.hops.forEach(hop => {
-    let hopList = document.querySelector('.hops')
+    let hopList = document.querySelector('.hops') 
     let hopName = document.createElement('li')
-    hopName.innerText = 
+    hopName.innerText = `Name: ${hop.name}, Amount: ${hop.amount.value} ${hop.amount.unit}, Add: ${hop.add}`
     hopList.appendChild(hopName)
   })
   let yeast = document.querySelector('.yeast')
@@ -22,8 +22,8 @@ function addBeerData(data) {
 }
 document.querySelector('form a').addEventListener('click', onSubmit)
 function onSubmit(event){
-  document.querySelector('.item').classList.remove('hidden'))
   event.preventDefault()
+  document.querySelector('.item').classList.remove('hidden')
   fetch(randomUrl)
   .then(response => response.json())
   .then(response => {

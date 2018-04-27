d654e878bd024401d4a771ce0096ce9cf7c04c5c
Jacob Hinkston <JacobHinkston@Gmail.com>
2018-04-27 14:19:06 -0600

    Finished js

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 randomUrl = 'https://api.punkapi.com/v2/beers/random'
-fetch(randomUrl)
-  .then(response => response.json())
-  .then(response => {
-    console.log(response)
-    addBeerData(response)
-  })
 
 function addBeerData(data) {
-  document.querySelector('.beer-name').innerText = `${data[0].name} - ${data[0].abv} ABV - ${data[0].ibu} IBU`
+  document.querySelector('.beer-name').innerText = 
   document.querySelector('.description').innerText = data[0].description
   data[0].ingredients.malt.forEach(malt => {
     let maltList = document.querySelector('.malt')
     let maltName = document.createElement('li')
-    maltName.innerText = `Name: ${malt.name}, Amount: ${malt.amount.value} ${malt.amount.unit}`
+    maltName.innerText = 
     maltList.appendChild(maltName)
   })
   data[0].ingredients.hops.forEach(hop => {
     let hopList = document.querySelector('.hops')
     let hopName = document.createElement('li')
-    hopName.innerText = `Name: ${hop.name}, Amount: ${hop.amount.value} ${hop.amount.unit}, Add: ${hop.add}`
+    hopName.innerText = 
     hopList.appendChild(hopName)
   })
   let yeast = document.querySelector('.yeast')
@@ -26,3 +20,14 @@ function addBeerData(data) {
   yeastName.innerText = data[0].ingredients.yeast
   yeast.appendChild(yeastName)
 }
+document.querySelector('form a').addEventListener('click', onSubmit)
+function onSubmit(event){
+  document.querySelector('.item').classList.remove('hidden'))
+  event.preventDefault()
+  fetch(randomUrl)
+  .then(response => response.json())
+  .then(response => {
+    addBeerData(response)
+  })
+}
+

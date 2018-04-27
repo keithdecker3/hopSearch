196a0d9af72c59362f8fa9807db8ec72b5c0b697
Christopher Nemeth <cnemeth1@gmail.com>
2018-04-27 13:40:06 -0600

    Updated Project

---
+++ app.js
@@ -0,0 +1,28 @@
+randomUrl = 'https://api.punkapi.com/v2/beers/random'
+fetch(randomUrl)
+  .then(response => response.json())
+  .then(response => {
+    console.log(response)
+    addBeerData(response)
+  })
+
+function addBeerData(data) {
+  document.querySelector('.beer-name').innerText = `${data[0].name} - ${data[0].abv} ABV - ${data[0].ibu} IBU`
+  document.querySelector('.description').innerText = data[0].description
+  data[0].ingredients.malt.forEach(malt => {
+    let maltList = document.querySelector('.malt')
+    let maltName = document.createElement('li')
+    maltName.innerText = `Name: ${malt.name}, Amount: ${malt.amount.value} ${malt.amount.unit}`
+    maltList.appendChild(maltName)
+  })
+  data[0].ingredients.hops.forEach(hop => {
+    let hopList = document.querySelector('.hops')
+    let hopName = document.createElement('li')
+    hopName.innerText = `Name: ${hop.name}, Amount: ${hop.amount.value} ${hop.amount.unit}, Add: ${hop.add}`
+    hopList.appendChild(hopName)
+  })
+  let yeast = document.querySelector('.yeast')
+  let yeastName = document.createElement('li')
+  yeastName.innerText = data[0].ingredients.yeast
+  yeast.appendChild(yeastName)
+}
